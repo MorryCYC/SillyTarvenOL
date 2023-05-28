9f41db7aecac1b89d65b507f392ba5949bf73a97
maver <kentucky@posteo.de>
2023-05-28 15:55:03 +0200

    Add prompt manager setup

--- public/script.js
+++ public/script.js
@@ -81,6 +81,7 @@ import {
 import {
     setOpenAIMessageExamples,
     setOpenAIMessages,
+    setupOpenAIPromptManager,
     prepareOpenAIMessages,
     sendOpenAIRequest,
     loadOpenAISettings,
@@ -4934,6 +4935,7 @@ async function getSettings(type) {
 
         // OpenAI
         loadOpenAISettings(data, settings);
+                setupOpenAIPromptManager(settings);
 
         // Horde
         loadHordeSettings(settings);
