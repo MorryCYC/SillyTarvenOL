aa1088a178906b73439318f26f8feb5c4260c321
maver <kentucky@posteo.de>
2023-06-27 20:40:17 +0200

    Fix prompt-manager being rendered twice.

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -586,7 +586,7 @@ function prepareOpenAIMessages({
                                          quietPrompt,
                                          extensionPrompts,
                                          cyclePrompt
-                                     } = {}) {
+                                     } = {}, dryRun) {
     const prompts = promptManager.getPromptCollection();
     const chatCompletion = new ChatCompletion();
     const userSettings = promptManager.serviceSettings;
@@ -667,7 +667,7 @@ function prepareOpenAIMessages({
         promptManager.populateTokenHandler(chatCompletion.getMessages());
 
         // All information are up-to-date, render without dry-run.
-        promptManager.render(false);
+        if (false === dryRun) promptManager.render(false);
     }
 
     const chat = chatCompletion.getChat();
